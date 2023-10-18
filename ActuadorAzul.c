b32901b5aff9d2fa1f7e48edf03c45eaf57d81c4
GARCIAA27 <55242507+GARCIAA27@users.noreply.github.com>
2023-10-17 21:38:49 -0600

    Actuiador led azul

---
+++ ActuadorAzul.c
@@ -0,0 +1,474 @@
+/*
+
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+
+ * Copyright 2016-2021 NXP
+
+ * All rights reserved.
+
+ *
+
+ * SPDX-License-Identifier: BSD-3-Clause
+
+ */
+
+
+
+#include "fsl_debug_console.h"
+
+#include "fsl_flexcan.h"
+
+#include "fsl_gpio.h"
+
+#include "pin_mux.h"
+
+#include "clock_config.h"
+
+#include "board.h"
+
+
+
+/*******************************************************************************
+
+ * Definitions
+
+ ******************************************************************************/
+
+#define EXAMPLE_CAN            CAN0
+
+#define EXAMPLE_CAN_CLK_SOURCE (kFLEXCAN_ClkSrc1)
+
+#define EXAMPLE_CAN_CLK_FREQ   CLOCK_GetFreq(kCLOCK_BusClk)
+
+/* Set USE_IMPROVED_TIMING_CONFIG macro to use api to calculates the improved CAN / CAN FD timing values. */
+
+#define USE_IMPROVED_TIMING_CONFIG (1U)
+
+#define RX_MESSAGE_BUFFER_NUM      (9)
+
+#define TX_MESSAGE_BUFFER_NUM      (8)
+
+#define DLC                        (8)
+
+
+
+/* Fix MISRA_C-2012 Rule 17.7. */
+
+#define LOG_INFO (void)PRINTF
+
+/*******************************************************************************
+
+ * Prototypes
+
+ ******************************************************************************/
+
+
+
+/*******************************************************************************
+
+ * Variables
+
+ ******************************************************************************/
+
+volatile bool txComplete = false;
+
+volatile bool rxComplete = false;
+
+flexcan_handle_t flexcanHandle;
+
+flexcan_mb_transfer_t txXfer, rxXfer;
+
+#if (defined(USE_CANFD) && USE_CANFD)
+
+flexcan_fd_frame_t txFrame, rxFrame;
+
+#else
+
+flexcan_frame_t txFrame, rxFrame;
+
+#endif
+
+
+
+/*******************************************************************************
+
+ * Code
+
+ ******************************************************************************/
+
+/*!
+
+ * @brief FlexCAN Call Back function
+
+ */
+
+static FLEXCAN_CALLBACK(flexcan_callback)
+
+{
+
+    switch (status)
+
+    {
+
+        /* Process FlexCAN Rx event. */
+
+        case kStatus_FLEXCAN_RxIdle:
+
+            if (RX_MESSAGE_BUFFER_NUM == result)
+
+            {
+
+                rxComplete = true;
+
+            }
+
+            break;
+
+
+
+        /* Process FlexCAN Tx event. */
+
+        case kStatus_FLEXCAN_TxIdle:
+
+            if (TX_MESSAGE_BUFFER_NUM == result)
+
+            {
+
+                txComplete = true;
+
+            }
+
+            break;
+
+
+
+        default:
+
+            break;
+
+    }
+
+}
+
+
+
+void configureRGBLED(void) {
+
+    // Iniciar clocks
+
+    CLOCK_EnableClock(kCLOCK_PortB);
+
+    CLOCK_EnableClock(kCLOCK_PortE);
+
+
+
+    // Definir configuración de pines
+
+    gpio_pin_config_t led_config = {
+
+        kGPIO_DigitalOutput,
+
+        0,
+
+    };
+
+
+
+    GPIO_PinInit(GPIOB, 21, &led_config); // BLue LED
+
+    GPIO_PinInit(GPIOB, 22, &led_config); // Red LED
+
+    GPIO_PinInit(GPIOE, 26, &led_config); // Green LED
+
+}
+
+
+
+void redLEDOff(void) {
+
+    GPIO_PortSet(GPIOB, 1u << 22); // Blue LED
+
+}
+
+
+
+void redLEDOn(void) {
+
+    GPIO_PortClear(GPIOB, 1u << 22); // Blue LED
+
+}
+
+void blueLEDOff(void) {
+
+    GPIO_PortSet(GPIOB, 1u << 21); // Red LED
+
+}
+
+void blueLEDOn(void) {
+
+    GPIO_PortClear(GPIOB, 1u << 21); // Red LED
+
+}
+
+void greenLEDOff(void) {
+
+    GPIO_PortSet(GPIOE, 1u << 26); // Green LED
+
+}
+
+
+
+void greenLEDOn(void) {
+
+    GPIO_PortClear(GPIOE, 1u << 26); // Green LED
+
+}
+
+void Configure_CAN_Pins ()
+
+{
+
+	PORT_SetPinMux(PORTB, 18, kPORT_MuxAlt2); //Tx
+
+	PORT_SetPinMux(PORTB, 19, kPORT_MuxAlt2); //Rx
+
+}
+
+void readTemp(uint32_t temp)
+
+{
+
+    if(temp < 28)
+
+    {
+
+    	blueLEDOn();
+
+    }
+
+    else
+    {
+
+		redLEDOff();
+
+		blueLEDOff();
+
+		greenLEDOff;
+
+    }
+
+}
+
+/*!
+
+ * @brief Main function
+
+ */
+
+int main(void)
+
+{
+
+    flexcan_config_t flexcanConfig;
+
+    flexcan_rx_mb_config_t mbConfig;
+
+    uint32_t rx0;
+
+    uint32_t rx1;
+
+
+
+    /* Initialize board hardware. */
+
+    BOARD_InitBootPins();
+
+    BOARD_InitBootClocks();
+
+    BOARD_InitDebugConsole();
+
+    Configure_CAN_Pins ();
+
+    configureRGBLED();
+
+    /* Init FlexCAN module. */
+
+    /*
+
+     * flexcanConfig.clkSrc                 = kFLEXCAN_ClkSrc0;
+
+     * flexcanConfig.baudRate               = 1000000U;
+
+     * flexcanConfig.baudRateFD             = 2000000U;
+
+     * flexcanConfig.maxMbNum               = 16;
+
+     * flexcanConfig.enableLoopBack         = false;
+
+     * flexcanConfig.enableSelfWakeup       = false;
+
+     * flexcanConfig.enableIndividMask      = false;
+
+     * flexcanConfig.disableSelfReception   = false;
+
+     * flexcanConfig.enableListenOnlyMode   = false;
+
+     * flexcanConfig.enableDoze             = false;
+
+     */
+
+    FLEXCAN_GetDefaultConfig(&flexcanConfig);
+
+
+
+#if defined(EXAMPLE_CAN_CLK_SOURCE)
+
+    flexcanConfig.clkSrc = EXAMPLE_CAN_CLK_SOURCE;
+
+#endif
+
+
+
+
+
+#if (defined(USE_IMPROVED_TIMING_CONFIG) && USE_IMPROVED_TIMING_CONFIG)
+
+    flexcan_timing_config_t timing_config;
+
+    memset(&timing_config, 0, sizeof(flexcan_timing_config_t));
+
+
+
+    if (FLEXCAN_CalculateImprovedTimingValues(EXAMPLE_CAN, flexcanConfig.baudRate, EXAMPLE_CAN_CLK_FREQ,
+
+                                              &timing_config))
+
+    {
+
+        /* Update the improved timing configuration*/
+
+        memcpy(&(flexcanConfig.timingConfig), &timing_config, sizeof(flexcan_timing_config_t));
+
+    }
+
+    else
+
+    {
+
+        LOG_INFO("No found Improved Timing Configuration. Just used default configuration\r\n\r\n");
+
+    }
+
+#endif
+
+
+
+
+
+    FLEXCAN_Init(EXAMPLE_CAN, &flexcanConfig, EXAMPLE_CAN_CLK_FREQ);
+
+
+
+
+
+
+
+    /* Setup Rx Message Buffer. */
+
+    mbConfig.format = kFLEXCAN_FrameFormatStandard;
+
+    mbConfig.type   = kFLEXCAN_FrameTypeData;
+
+    mbConfig.id     = FLEXCAN_ID_STD(0x8);
+
+#if (defined(USE_CANFD) && USE_CANFD)
+
+    FLEXCAN_SetFDRxMbConfig(EXAMPLE_CAN, RX_MESSAGE_BUFFER_NUM, &mbConfig, true);
+
+#else
+
+    FLEXCAN_SetRxMbConfig(EXAMPLE_CAN, RX_MESSAGE_BUFFER_NUM, &mbConfig, true);
+
+#endif
+
+
+
+
+
+/* Setup Tx Message Buffer. */
+
+    FLEXCAN_SetTxMbConfig(EXAMPLE_CAN, TX_MESSAGE_BUFFER_NUM, true);
+
+
+
+    /* Create FlexCAN handle structure and set call back function. */
+
+    FLEXCAN_TransferCreateHandle(EXAMPLE_CAN, &flexcanHandle, flexcan_callback, NULL);
+
+
+
+
+
+    /* Prepare Tx Frame for sending. */
+
+    txFrame.format = (uint8_t)kFLEXCAN_FrameFormatStandard;
+
+    txFrame.type   = (uint8_t)kFLEXCAN_FrameTypeData;
+
+    txFrame.id     = FLEXCAN_ID_STD(0x5);
+
+    txFrame.length = (uint8_t)DLC;
+
+
+
+
+
+
+
+    /* Start receive data through Rx Message Buffer. */
+
+    rxXfer.mbIdx = (uint8_t)RX_MESSAGE_BUFFER_NUM;
+
+
+
+uint32_t data = 0;
+
+    while (1)
+
+    {
+
+    	rxXfer.frame = &rxFrame;
+
+		(void)FLEXCAN_TransferReceiveNonBlocking(EXAMPLE_CAN, &flexcanHandle, &rxXfer);
+
+
+
+		/* Waiting for Rx Message finish. */
+
+		while ((!rxComplete))
+
+		{
+
+		};
+
+
+
+		LOG_INFO("rx word0 = %d\r\n", rxFrame.dataWord0);
+
+		LOG_INFO("rx word1 = %d\r\n", rxFrame.dataWord1);
+
+
+
+		rxComplete = false;
+
+		data = rxFrame.dataWord0;
+
+		readTemp(data);
+
+
+
+    }
+
+
+
+}
